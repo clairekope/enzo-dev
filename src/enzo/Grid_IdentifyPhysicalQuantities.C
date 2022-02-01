828023d49d1c631f0612655fa1d684949e5431f0
Claire Kopenhafer <kopenhaf@msu.edu>
2022-02-01 17:19:41 -0500

    Caught an overload problem. I should test...

--- src/enzo/Grid_IdentifyPhysicalQuantities.C
+++ src/enzo/Grid_IdentifyPhysicalQuantities.C
@@ -79,8 +79,7 @@ int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
 }
 
 int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
-                                     int &Vel2Num, int &Vel3Num, int &TENum,
-                                     int &CRENum, int &CRF1Num, int &CRF2Num, int &CRF3Num)
+             int &Vel2Num, int &Vel3Num, int &TENum, int &CRENum, int &CRF1Num, int &CRF2Num, int &CRF3Num)
 {
 
   this->IdentifyPhysicalQuantities(DensNum, GENum, Vel1Num, Vel2Num, Vel3Num, TENum);
@@ -104,7 +103,6 @@ int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
         ENZO_FAIL("Cannot Find Cosmic Ray Energy Flux");
     }
   }
-
   return SUCCESS;
 }
 
@@ -180,14 +178,81 @@ int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
   return SUCCESS;
 }
 
+int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num, 
+				     int &Vel2Num, int &Vel3Num, int &TENum,
+				     int &B1Num, int &B2Num, int &B3Num, int &PhiNum,
+             int &CRENum, int &CRF1Num, int &CRF2Num, int &CRF3Num)
+{
 
-int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
-             int &Vel2Num, int &Vel3Num, int &TENum,
-             int &B1Num, int &B2Num, int &B3Num, int &PhiNum, 
-             int &CRENum, int &CRF1Num, int &CRF2Num, int &CRF3Num){
+  DensNum = GENum = Vel1Num = Vel2Num = Vel3Num = TENum = 0;
+    
+  /* Find Density, if possible. */
 
-  this->IdentifyPhysicalQuantities(DensNum,GENum,Vel1Num,Vel2Num,Vel3Num,
-				   TENum,B1Num,B2Num,B3Num,PhiNum);
+  if ((DensNum = FindField(Density, FieldType, NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find density.");
+  }
+
+  /* Find Total energy, if possible. */
+
+  if ((TENum = FindField(TotalEnergy, FieldType, NumberOfBaryonFields)) < 0) {
+    if( EquationOfState != 1)
+      ENZO_FAIL("Cannot find total energy.");
+  }
+
+  /* Find gas energy, if possible. */
+
+  if (DualEnergyFormalism == TRUE) {
+    if ((GENum = FindField(InternalEnergy, FieldType,
+			   NumberOfBaryonFields)) < 0) {
+            ENZO_FAIL("Cannot find gas energy.");
+    }
+  }
+
+  /* Find Velocity1, if possible. */
+   
+  if ((Vel1Num = FindField(Velocity1, FieldType, NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find Velocity1.");
+  }
+
+  /* Find Velocity2, if possible. */
+
+  if (MaxVelocityIndex>1)
+      if ((Vel2Num = FindField(Velocity2, FieldType, 
+			   NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find Velocity2.");
+  }
+
+  /* Find Velocity3, if possible. */
+
+  if (MaxVelocityIndex>2)
+      if ((Vel3Num = FindField(Velocity3, FieldType, 
+                      NumberOfBaryonFields)) == 0) {
+          ENZO_FAIL("Cannot find Velocity3.");
+      }
+
+  if ( ! UseMHD ) {
+    return SUCCESS;
+  }
+
+  /* Find magnetic field components */
+
+  if ((B1Num = FindField(Bfield1, FieldType, NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find Bfield1.");
+  }
+
+  if ((B2Num = FindField(Bfield2, FieldType, NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find Bfield2.");
+  }
+   
+  if ((B3Num = FindField(Bfield3, FieldType, NumberOfBaryonFields)) < 0) {
+        ENZO_FAIL("Cannot find Bfield3.");
+  }
+
+  if ( HydroMethod == MHD_RK ){
+      if ((PhiNum = FindField(PhiField, FieldType, NumberOfBaryonFields)) < 0) {
+            ENZO_FAIL("Cannot find Phi field.");
+      }
+  }
 
   CRENum = 0;
   CRF1Num = 0;
@@ -206,9 +271,8 @@ int grid::IdentifyPhysicalQuantities(int &DensNum, int &GENum, int &Vel1Num,
         ENZO_FAIL("Cannot Find Cosmic Ray Energy Flux");
     }
   }
-
+  
   return SUCCESS;
-
 }
 
 int grid::IdentifyDrivingFields(int &Drive1Num, int &Drive2Num, int &Drive3Num)
